09322701653dab0649446ab927fb70089b5e61e0
TetraMeister <lukasz3s.praca@gmail.com>
2026-02-27 23:11:27 +0100

    Task03, Task04, Task05 marked as done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,32 @@
-import {files} from './data.js';
+import { files } from './data.js';
+const newPEl = document.createElement('p');
 
+let sum = 0;
+
+files.forEach((e) => {
+  const { size: { length }, size: { unit = 'B' } } = e;
+
+  sum += convertToB(unit, length);
+});
+
+newPEl.textContent = sum + ' B';
+document.querySelector('body').appendChild(newPEl);
+
+
+function convertToB(unit, size) {
+  if (unit === 'B') {
+    return size;
+  };
+
+  if (unit === 'KB') {
+    return size * 1024
+  };
+
+  if (unit === 'MB') {
+    return size * 1024 * 1024
+  };
+
+  if (unit === 'GB') {
+    return size * 1024 * 1024 * 1024
+  };
+}
--- 05/app.js
+++ 05/app.js
@@ -1,8 +1,11 @@
+import Junior from './classes/Junior';
+import Mid from './classes/Mid';
 import Programmer from './classes/Programmer';
+import Senior from './classes/Senior';
 
 const features = {
-    skills: 10, 
-    experience: 10, 
+    skills: 10,
+    experience: 10,
     willingness: 10,
 }
 
@@ -11,6 +14,27 @@ const task = {
     size: 1,
 }
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
+const programmer = new Programmer(features);
+const time = programmer.getApproximateTimeTaskDoneInHours(task);
 console.log(time);
+
+const juniorFeatures = new Junior(features);
+const juniorClean = new Junior();
+const timeJuniorF = juniorFeatures.getApproximateTimeTaskDoneInHours(task);
+const timeJuniorC = juniorClean.getApproximateTimeTaskDoneInHours(task);
+console.log(`Time for set features junior: ${timeJuniorF}`);
+console.log(`Time for unset features junior: ${timeJuniorC}`);
+
+const midFeatures = new Mid(features);
+const midClean = new Mid();
+const timeMidF = midFeatures.getApproximateTimeTaskDoneInHours(task);
+const timeMidC = midClean.getApproximateTimeTaskDoneInHours(task);
+console.log(`Time for set features mid: ${timeMidF}`);
+console.log(`Time for unset features mid: ${timeMidC}`);
+
+const seniorFeatures = new Senior(features);
+const seniorClean = new Senior();
+const timeSeniorF = seniorFeatures.getApproximateTimeTaskDoneInHours(task);
+const timeSeniorC = seniorClean.getApproximateTimeTaskDoneInHours(task);
+console.log(`Time for set features senior: ${timeSeniorF}`);
+console.log(`Time for unset features senior: ${timeSeniorC}`);
